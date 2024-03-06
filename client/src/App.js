accd65f7704b0d3267bc79cc6811a99c35c290d4
George Hughes <groberth@live.com>
2024-03-05 17:52:41 -0800

    Gathering and displaying user data from the spotify api

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react';
-import { accessToken, logout } from './spotify';
+import { accessToken, logout, getCurrentUserProfile } from './spotify';
+import { catchErrors } from './utils';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
     const [token, setToken] = useState(null);
+    const [profile, setProfile] = useState(null);
     useEffect(() => {
         setToken(accessToken);
+        const fetchData = async() => {
+            const { data } = await getCurrentUserProfile();
+            setProfile(data);
+        }
+        catchErrors(fetchData());
     }, []);
 
     return (
@@ -23,6 +30,15 @@ function App() {
                     <>
                         <h1>Logged in!</h1>
                         <button onClick={logout}>Log Out</button>
+                        {profile && (
+                            <div>
+                                <h1>{profile.display_name}</h1>
+                                <p>{profile.followers.total} Followers</p>
+                                {profile.images.length && profile.images[0].url && (
+                                    <img src={profile.images[0].url} alt="Avatar" />
+                                )}
+                            </div>
+                        )}
                     </>
                 )}
             </header>
--- client/src/spotify.js
+++ client/src/spotify.js
@@ -112,3 +112,18 @@ const getAccessToken = () => {
 };
 
 export const accessToken = getAccessToken();
+
+/**
+ * Axios global request headers
+ * https://github.com/axios/axios#global-axios-defaults
+ */
+axios.defaults.baseURL = 'https://api.spotify.com/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${accessToken}`;
+axios.defaults.headers['Content-Type'] = 'application/json';
+
+/**
+ * Get Current User's Profile
+ * https://developer.spotify.com/documentation/web-api/reference/#endpoint-get-current-users-profile
+ * @returns {Promise}
+ */
+export const getCurrentUserProfile = () => axios.get('/me');
