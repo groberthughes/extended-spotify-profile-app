fd2ccfdeebeaaa6965c4263764355df9fa4fbf45
George Hughes <groberth@live.com>
2024-03-07 17:23:12 -0800

    Set up page routing and styled componets

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    useLocation,
+  } from 'react-router-dom';
 import { accessToken, logout, getCurrentUserProfile } from './spotify';
 import { catchErrors } from './utils';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+// Scroll to top of page when changing routes
+function ScrollToTop() {
+    const { pathname } = useLocation();
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+  }
 
 function App() {
     const [token, setToken] = useState(null);
@@ -27,19 +44,33 @@ function App() {
                     Log in to Spotify
                 </a>
                 ) : (
-                    <>
-                        <h1>Logged in!</h1>
-                        <button onClick={logout}>Log Out</button>
-                        {profile && (
-                            <div>
-                                <h1>{profile.display_name}</h1>
-                                <p>{profile.followers.total} Followers</p>
-                                {profile.images.length && profile.images[0].url && (
-                                    <img src={profile.images[0].url} alt="Avatar" />
-                                )}
-                            </div>
-                        )}
-                    </>
+                    <Router>
+                        <ScrollToTop />
+                        <Routes>
+                            <Route path="/top-artists" element={<h1>Top Artists</h1>} />
+                            <Route path="/top-tracks" element={<h1>Top Tracks</h1>} />
+                            <Route path="/playlists" element={<h1>Playlists</h1>} />
+                            <Route path="/playlists/:id" element={<h1>Playlist</h1>} />
+                            <Route
+                                path="/"
+                                element={
+                                    <>
+                                    <button onClick={logout}>Log Out</button>
+
+                                    {profile && (
+                                        <div>
+                                        <h1>{profile.display_name}</h1>
+                                        <p>{profile.followers.total} Followers</p>
+                                        {profile.images.length && profile.images[0].url && (
+                                            <img src={profile.images[0].url} alt="Avatar" />
+                                        )}
+                                        </div>
+                                    )}
+                                    </>
+                                }
+                            />
+                        </Routes>
+                    </Router>
                 )}
             </header>
         </div>
